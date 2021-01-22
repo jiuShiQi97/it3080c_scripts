c35a8632559595690ef6854372b4d8d927b5dade
jiuShiQi97 <1018726071@qq.com>
2021-01-22 08:07:58 -0500

    Mu first commit

---
+++ bash/myfirstscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This script outputs the IP addres and hostname of machine
+
+echo 'This is a script. Hello!'
+
+greeting="this is a script. Hello"
+echo $greeting, thanks for joining!
+echo '$greeting, You own me $20'
+echo "$greeting, thanks"
+echo "$greeting, you own me $20"
+
+echo Machine Type: $MACHTYPE
+echo Hostname: $HOSTNAME
+echo Working Dir: $PWD
+echo Home Dir: $HOME
