3e4b00726dbe5a393806c64c88e240abd5e392b0
jiuShiQi97 <1018726071@qq.com>
2021-02-05 12:37:50 -0500

    add sysinfo to bash

---
+++ bash/sysinfo.ps1
@@ -0,0 +1,17 @@
+﻿$Hello = "Hello,powersell!"
+write-Host($Hello)
+function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+Write-Host(getIP)
+
+
+$IP = getIP
+$Date = Get-Date
+$Host1 = $Host.Version.Major
+$Hostname = Get-WmiObject Win32_ComputerSystem | select Name
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $Hostname. PowerShell version $Host1. Today's Date is $Date"
+
+Write-Host($Body)
+
+#Send-MailMessage -To "wenhanja@mail.uc.edu" -From "wenhanja@mail.uc.edu" -Subject "IT3038c windows SysInfo" -Body $Body -smtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
