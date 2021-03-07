2ebab5ac387a49a3bb9a93c1b41720816c3f534f
jiuShiQi97 <1018726071@qq.com>
2021-03-08 00:07:46 +0800

    powershell in lab 7

---
+++ powershell/counter.ps1
@@ -0,0 +1,14 @@
+﻿$Machines = 'LAPTOP-6OBAAP10'
+$Logfile = "C:\Users\JSQ97\Script\it3080c_scripts\powershell\logs\counterdata.log"
+$Date = Get-Date
+
+Foreach($machine in $Machines) {
+    #$RCounters = Get-Counter -ListSet * -ComputerName $machine
+    #"There are {0} counters on {1}" -f  $RCounters.count,  ($machine)
+    $pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 5).CounterSamples.CookedValue
+    $sample = 1
+    foreach($p in $pt){
+        "{3} Sample {2}: CPU is at {0}% on {1}" -f [int]$p, $machine, $sample, $Date | Out-File -Append $Logfile
+        $sample++
+    }   
+}
---
+++ powershell/rando.ps1
@@ -0,0 +1,7 @@
+﻿$RANDO = 0
+$Logfile = "C:\Users\JSQ97\Script\it3080c_scripts\powershell\logs\rando.log"
+for($i = 0; $i -lt 5; $i++){
+    $RANDO = Get-Random -Maximum 1000 -Minimum 1
+    Write-Host($RANDO)
+    Add-Content $Logfile "INFO: Random number is ${RANDO}"
+}
